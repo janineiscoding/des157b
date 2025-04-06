c084d147293d14c557432085f0fbca74624b06bc
Janine Kong <janine@Janines-MacBook-Air-2.local>
2025-04-06 02:05:22 -0700

    portal edit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
     'use strict';
 
     const button = document.querySelector('button');
-    const headerImg = document.querySelector('header > img');
+    const headerImg = document.querySelectorAll('header > img');
     const mainImg = document.querySelectorAll('main img');
     const footer = document.querySelector('footer');
     let mode = 'default';
@@ -10,17 +10,22 @@
     button.addEventListener('click', function() {
         if (mode === 'default') {
             button.innerHTML = '<img src="images/hide-toggle.svg" alt="hide toggle">';
-            headerImg.className = 'show';
-            for (const images of mainImg) {
+            for (const images of headerImg) {
                 images.className = 'show';
+            }
+            for (const images2 of mainImg) {
+                images2.className = 'show';
             }
             footer.className = 'show';
             mode = 'annotate';
         } else {
             button.innerHTML = '<img src="images/see-toggle.svg" alt="see toggle">';
+            for (const images3 of headerImg) {
+                images3.className = 'hide';
+            }
             headerImg.className = 'hide';
-            for (const images2 of mainImg) {
-                images2.className = 'hide';
+            for (const images4 of mainImg) {
+                images4.className = 'hide';
             }
             footer.className = 'hide';
             mode = 'default'
