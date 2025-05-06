0e81f8bfcd51eac8fb392653ed96c288069b495a
Janine Kong <janine@campus-035-191.ucdavis.edu>
2025-05-06 13:13:53 -0700

    capstone proposal

--- stuido3/script.js
+++ stuido3/script.js
@@ -1,5 +1,18 @@
 (function(){
     'use strict'
     console.log('reading js');
-    
+
+    var granimInstance = new Granim({
+        element: '#granim-canvas',
+        name: 'granim',
+        opacity: [1, 1],
+        states : {
+            "default-state": {
+                gradients: [
+                    ['#834D9B', '#D04ED6'],
+                    ['#1CD8D2', '#93EDC7']
+                ]
+            }
+        }
+     });
 })();
